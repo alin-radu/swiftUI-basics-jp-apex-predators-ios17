13f81f2bb0ee0a1cf3dd1b8f2573d5d3f9ed8319
Alin RADU <alin.radu.cristian@gmail.com>
2024-02-04 13:35:18 +0200

    Initial Commit

---
+++ JPApexPredators-ios17/JPApexPredators_ios17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators_ios17App.swift
+//  JPApexPredators-ios17
+//
+//  Created by Alin RADU on 04.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators_ios17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
